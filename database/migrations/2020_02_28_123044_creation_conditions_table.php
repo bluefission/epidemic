266ce95c1f911009eae3b3dad03661e23dcdb859
Devon Scott <dscott@bluefission.com>
2020-02-28 12:58:21 -0500

    added condition migration

---
+++ database/migrations/2020_02_28_123044_creation_conditions_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreationConditionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+
+        Schema::create('protocol_qualification', function(Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('protocol_id')
+                ->foreign('protocol_id')
+                ->references('id')->on('protocols')
+                ->onDelete('cascade')
+                ->nullable();
+            $table->uuid('qualification_id');
+            $table->timestamps();
+        });
+
+        Schema::create('protocol_question_qualification', function(Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('protocol_question_id')
+                ->foreign('protocol_question_id')
+                ->references('id')->on('protocol_questions')
+                ->onDelete('cascade')
+                ->nullable();
+            $table->uuid('qualification_id');
+            $table->timestamps();
+        });
+
+        Schema::create('protocol_conditions', function(Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('operator');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('protocol_qualification');
+        Schema::dropIfExists('protocol_question_qualification');
+        Schema::dropIfExists('protocol_conditions');
+
+    }
+}
