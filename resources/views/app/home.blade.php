49cc61146fff02ac8f9307ca393a9db4f79c3a83
Devon Scott <dscott@bluefission.com>
2020-02-29 14:51:52 -0500

    added mockup images to the demo homepage

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -2,12 +2,68 @@
 
 @section('content')
 
-            <div id="google-map5" style="height: 400px; margin-bottom: 20px;" class="gmap"></div>
+            <div id="google-map-outbreak" style="height: 400px; margin-bottom: 20px;display:none;" class="gmap"></div>
 
             <div class="content-wrap">
                 <div class="container clearfix">
 
-                    <p class="nobottommargin">Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Duis mollis, est non commodo luctus.Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Nulla vitae elit libero, a pharetra augue.</p>
+                    <div class="onboarding" id="onboarding-step-1">
+                        <img src="https://github.com/bluefission/epidemic/raw/master/resources/views/onboard-step-1-mockup.png" />
+                    </div>
+                    <div class="onboarding" id="onboarding-step-2">
+                        <img src="https://github.com/bluefission/epidemic/raw/master/resources/views/onboard-step-2-mockup.png" />
+                    </div>
+                    <div class="onboarding" id="onboarding-step-3">
+                        <img src="https://github.com/bluefission/epidemic/raw/master/resources/views/onboard-step-3-mockup.png" />
+                    </div>
+                    <div class="onboarding" id="onboarding-step-4">
+                        <img src="https://github.com/bluefission/epidemic/raw/master/resources/views/invite-mockup.png" />
+                    </div>
+                    <div class="onboarding" id="onboarding-step-5">
+                        <img src="https://github.com/bluefission/epidemic/raw/master/resources/views/main-survey-mockup.png" />
+                    </div>
+                    <div class="onboarding" id="onboarding-step-6">
+                        <img src="https://github.com/bluefission/epidemic/raw/master/resources/views/results-mockup.png" />
+                    </div>
+                    <div class="onboarding" id="onboarding-step-7">
+                        <img src="https://github.com/bluefission/epidemic/raw/master/resources/views/main-dashboard-mockup.png  " />
+                    </div>
+                    <div class="onboarding" id="onboarding-step-8">
+                        <img src="https://github.com/bluefission/epidemic/raw/master/resources/views/deep-dive-mockup.png" />
+                    </div>
+
+                    <div id="section-home" class="recent-info heading-block title-center nobottomborder page-section">
+                        <h1>Track and Predict Contagious Outbreaks</h1>
+                        <span>Join the <span class="color">largest</span> public epidemic tracking community online</span>
+                    </div>
+
+                    <div class="center bottommargin recent-info">
+                        <div class="col_one_third">
+
+                            <form>
+                                <div class="col_full">
+                                    <label for="template-contactform-subject">Email Address <small>*</small></label>
+                                    <input type="text" id="template-contactform-subject" name="subject" value="" class="required sm-form-control valid">
+                                </div>
+                                <div class="clear"></div>
+
+                                <div class="col_full">
+                                    <label for="template-contactform-subject">Password <small>*</small></label>
+                                    <input type="password" id="template-contactform-subject" name="subject" value="" class="required sm-form-control valid">
+                                </div>
+                                <div class="clear"></div>
+                                <div class="col_full">
+                                    <a href="#" class="button button-3d button-teal button-xlarge nobottommargin"><i class="icon-star3"></i>Sign Up Now</a>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="col_two_third col_half">
+                            <p class="nobottommargin">
+                            Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Duis mollis, est non commodo luctus.Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Nulla vitae elit libero, a pharetra augue.</p>
+                        </div>
+                    </div>
+                    
+                    <div class="clear"></div>
                 </div>
             </div>
 
@@ -16,6 +72,14 @@
 @section('scripts')
 <!-- External JavaScripts
     ============================================= -->
+    <style>
+        .onboarding {
+            text-align: center;
+        }
+        .recent-info {
+            display: none;
+        }
+    </style>
     <script src="js/jquery.js"></script>
     <script src="js/plugins.js"></script>
     <script src="https://maps.google.com/maps/api/js?key={{ env('GOOGLE_MAP_KEY') }}"></script>
@@ -27,7 +91,24 @@
 
     <script>
 
-        $('#google-map5').gMap({
+        $(document).ready(function() {
+            $('.onboarding').hide();
+            $('.onboarding:first').fadeIn();
+            $('.onboarding').on('click', function(e) {
+                $(this).fadeOut(function() {
+                    var $next = $(this).next('.onboarding');
+                    if ( $next.length > 0 ) {
+                        $next.fadeIn();
+                    } else {
+                        $('#google-map-outbreak').fadeIn(function() {
+                            $('.recent-info').fadeIn();
+                        });
+                    }
+                });
+            });
+        });
+
+        $('#google-map-outbreak').gMap({
              address: 'North Carolina, USA',
              maptype: 'ROADMAP',
              zoom: 3,
